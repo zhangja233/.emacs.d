9e85f73b630f93c35b85409bc6bddbad1ef453cb
Ju-an Zhang <zja148740@gmail.com>
2021-12-09 13:29:04 -0500

    add some configs for magit

--- configs/init-magit.el
+++ configs/init-magit.el
@@ -1,4 +1,13 @@
 (use-package magit
 :ensure  t
+:init
+(global-auto-revert-mode) ; revert any buffer if the file changes on disk
   )
+
+; refresh the status buffer whenever a buffer within the corresponding repo
+(with-eval-after-load 'magit-mode
+  (add-hook 'after-save-hook 'magit-after-save-refresh-status t))
+
+
+
 (provide 'init-magit)
