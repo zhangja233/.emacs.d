57511adb103a648f96375ce10165e1ab5e1f70ae
Ju-an Zhang <zja148740@gmail.com>
2021-12-18 17:07:04 -0500

    movement in help-mode

--- configs/init-help.el
+++ configs/init-help.el
@@ -1,5 +1,8 @@
 ;; finding help and documentations
 
+(define-key help-mode-map (kbd "f") 'help-go-forward)
+(define-key help-mode-map (kbd "b") 'help-go-back)
+
 (setq source-directory "~/lib/emacs/src")
 
 (provide 'init-help)
