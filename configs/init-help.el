89828c222d7f3863b9fbde70f8e92befac908b3e
Ju-an Zhang <zja148740@gmail.com>
2021-12-20 19:04:56 -0500

    get src-dir right

--- configs/init-help.el
+++ configs/init-help.el
@@ -3,6 +3,6 @@
 (define-key help-mode-map (kbd "f") 'help-go-forward)
 (define-key help-mode-map (kbd "b") 'help-go-back)
 
-(setq source-directory "~/lib/emacs/src")
+(setq find-function-C-source-directory "~/lib/emacs/src")
 
 (provide 'init-help)
