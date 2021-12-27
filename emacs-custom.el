895f42d363f499b5aca7c44e3e9f625a83195e9e
Ju-an Zhang <zja148740@gmail.com>
2021-12-27 18:30:32 -0500

    update emacs-custom on windows

--- emacs-custom.el
+++ emacs-custom.el
@@ -11,7 +11,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:foundry "PfEd" :slant normal :weight normal :height 221 :width normal))))
+ '(default ((t (:family "Microsoft YaHei Mono" :foundry "outline" :slant normal :weight normal :height 199 :width normal))))
  '(font-latex-sectioning-0-face ((t :height 301 :foreground "red" :weight bold)))
  '(font-latex-sectioning-1-face ((t (:height 301 :foreground "red" :weight bold))))
  '(font-latex-sectioning-2-face ((t (:height 281 :foreground "RoyalBlue4" :weight bold))))
