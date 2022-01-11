2188748c6563e16ac071ba524f6f3e59b911e88f
Ju-an Zhang <zja148740@gmail.com>
2022-01-11 10:30:24 -0500

    Make buffer-switching more versatile

--- configs/init-helm.el
+++ configs/init-helm.el
@@ -33,7 +33,7 @@
   (advice-add 'helm-buffers-sort-transformer :around 'helm-buffers-sort-transformer@donot-sort)
 
   (global-set-key (kbd "M-x") 'helm-M-x)
-  (global-set-key (kbd "C-,") 'helm-mini)
+  (global-set-key (kbd "C-,") 'helm-multi-files)
   (global-set-key (kbd "C-x f") 'helm-find-files)
   (global-set-key (kbd "M-s o") 'helm-occur)
   (define-key isearch-mode-map (kbd "C-o") 'helm-occur-from-isearch)
