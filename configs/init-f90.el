938f3c10e79052e5409f1f281fe3cdff2c064844
Ju-an Zhang <zja148740@gmail.com>
2022-05-20 19:08:19 -0400

    add f90-beginning-of-defun and f90-end-of-defun

--- configs/init-f90.el
+++ configs/init-f90.el
@@ -10,14 +10,39 @@
 '(progn
 (bind-keys :map f90-mode-map
 	   ("C-j" . newline-and-indent)
+	   ("RET" . newline-and-indent)
 	   ("C-;" . insert-exclamation)
-	   ("C-M-a" . f90-beginning-of-block)
-	   ("C-M-e" . f90-end-of-block)
+	   ("C-M-a" . f90-beginning-of-defun)
+	   ("C-M-e" . f90-end-of-defun)
 	   ("C-c C-f" . f90-next-block)
-	   ("C-c C-b" . f90-previous-block)))
-)
+	   ("C-c C-b" . f90-previous-block))
+
+(defconst f90-defun-re
+  (concat "\\(\\(?:block[ \t]*data\\|"
+          (regexp-opt '("function" "subroutine"))
+          "\\)\\_>\\)")
+  "Regexp potentially indicating a \"defun\" of F90 code.")
+
+(defun f90-beginning-of-defun (&optional arg)
+  (interactive "p")
+  (or (consp arg) (region-active-p) (push-mark))    
+  (re-search-backward (concat "^[\t ]*" f90-defun-re) nil 'move)
+  (back-to-indentation))
+
+(defun f90-end-of-defun (&optional arg)
+  (interactive "p")
+  (or (consp arg) (region-active-p) (push-mark))  
+  (re-search-forward (concat "^[\t ]*end[\t ]*" f90-defun-re) nil 'move)
+  (end-of-line))
+
+)) ; f90 ends here
+
 (setq f90-smart-end 'no-blink)
-(setq f90-critical-indent '4)
-(setq f90-program-indent '4)
+(setq f90-critical-indent 2)
+(setq f90-do-indent 2)
+(setq f90-if-indent 2)
+(setq f90-type-indent 2)
+(setq f90-program-indent 2)
+
 
 (provide 'init-f90)
