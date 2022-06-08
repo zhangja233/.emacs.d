fbc8310c7cf071d89a740bdb2229eb18a0659ee8
Ju-an Zhang <zja148740@gmail.com>
2022-06-07 21:22:06 -0400

    use lispy

--- configs/init-elisp.el
+++ configs/init-elisp.el
@@ -9,8 +9,13 @@
 	   ("M-DEL" . mark-defun)
 	   ("C-j" . newline-and-indent))
 
+(use-package lispy
+  :ensure t
+  :hook (emacs-lisp-mode . lispy-mode)
+  :config
+)
 
-; solve the problem that electric-indent-mode makes intent too much after return
+;; solve the problem that electric-indent-mode makes intent too much after return
 (defun electric-indent-mode-configure ()
   "Delete newline (?\n) from `electric-indent-chars'."
   (setq electric-indent-chars (delq 10 electric-indent-chars)))
