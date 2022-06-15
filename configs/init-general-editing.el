ea88ef8d9ab6aa1b29aafe7fc31ef80432a50dc6
zhangja233 <zja148740@gmail.com>
2022-06-14 20:36:55 -0400

    bind l to find-file in dired

--- configs/init-general-editing.el
+++ configs/init-general-editing.el
@@ -590,7 +590,8 @@ _d_: subtree
 
 
 (bind-keys :map dired-mode-map
-	   ("f" . find-file))
+	   ("f" . find-file)
+	   ("l" . find-file))
 (define-key dired-mode-map (kbd "C-/") (lambda() (interactive) (message "C-/ is disabled in dired")))
 (define-key dired-mode-map (kbd "SPC") 'browse-url-of-dired-file)
 (define-key dired-mode-map (kbd "E") 'wdired-change-to-wdired-mode)
