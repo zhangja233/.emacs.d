18deaf899b51c84329ac2e90f4161cdac6b414d9
Ju-an Zhang <zja148740@gmail.com>
2022-07-08 23:02:36 -0400

    add init-app

---
+++ configs/init-app.el
@@ -0,0 +1,24 @@
+;; general applications 
+
+(use-package ledger-mode
+  :ensure t
+  :init
+  (add-to-list 'auto-mode-alist
+	       '("ledger.*\\.dat$" . ledger-mode))
+  :bind
+  (:map ledger-mode-map
+	("C-;" . insert-single-dollar)))
+
+(use-package elfeed
+  :ensure t
+  :config
+  (setq elfeed-feeds
+	'(
+	  "https://terrytao.wordpress.com/feed/"
+	  "http://nullprogram.com/feed/"
+	  "https://batsov.com/atom.xml" ; bbatsov
+	  "https://sachachua.com/blog/feed/"
+          ;; "https://planet.emacslife.com/atom.xml"
+	  )))
+
+(provide 'init-app)
