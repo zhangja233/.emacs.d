b99cf5a55992f91fecd3e8577748e0b37af441e4
Ju-an Zhang <zja148740@gmail.com>
2022-07-09 20:55:06 -0400

    change font size

--- configs/init-linux.el
+++ configs/init-linux.el
@@ -1,10 +1,11 @@
 (when (eq system-type 'gnu/linux)
+  ;; this font size works well on ubuntu desktop
   (custom-set-faces
    ;; custom-set-faces was added by Custom.
    ;; If you edit it by hand, you could mess it up, so be careful.
    ;; Your init file should contain only one such instance.
    ;; If there is more than one, they won't work right.
-   '(default ((t (:foundry "outline" :slant normal :weight normal :height 201 :width normal)))))
+   '(default ((t (:foundry "outline" :slant normal :weight normal :height 165 :width normal)))))
   (set-face-attribute 'region nil :background "yellow")    
   (defun find-linux-dot-org()
     (interactive)
